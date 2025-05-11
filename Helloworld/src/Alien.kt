1bd7d88e361220b3fa29204e43ede9f44276b593
Debjit Saha <debjitsaha606@gmail.com>
2025-05-11 11:48:08 +0530

    addiiing practice code

---
+++ Helloworld/src/Alien.kt
@@ -0,0 +1,6 @@
+class Alien {
+    //var is variable
+    // to use constant use val
+    // using val we can constant object also
+    var name : String= "";
+}
---
+++ Helloworld/src/Alien1.java
@@ -0,0 +1,11 @@
+public class Alien1 {
+
+    private String name;
+
+    public void set(String name){
+        this.name= name;
+    }
+    public String get(){
+        return name;
+    }
+}
